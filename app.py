0d4a454967145e6c51f6a1f8f21e909e607afbd2
FaisalWaris777 <77573875+FaisalWaris777@users.noreply.github.com>
2023-01-08 16:03:44 +0530

    Add files via upload
    app.py-updated

--- app.py
+++ app.py
@@ -1,9 +1,93 @@
-from flask import Flask, render_template
+from flask import Flask,render_template,redirect,request
+import pickle
+import cv2
+import time
+from keras import models 
+import os
+import base64
+from PIL import Image
+import base64
+import io
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    print("hello")
-    
-hello()
+@app.route("/heart_disease")
+def heart_disease():
+    return render_template("heart_disease.html")
+
+@app.route('/heart_disease_result', methods=['POST'])
+def heart_disease_result():
+    age = request.form['age']
+    cpt = request.form['chest-pain-type']
+    mhra = request.form['maximum-heart-rate-achieved']
+    eia = request.form['exercise-induced-angina']
+    oldpeak = request.form['oldpeak']
+    nomv = request.form['number-of-major-vessels']
+    thal = request.form['thal']
+    user_input=[[age,cpt,mhra,eia,oldpeak,nomv,thal]]
+    # print(age)
+    model=""
+    with open('Heart_Disease_Prediction','rb') as file:
+      model = pickle.load(file)
+    output=model.predict(user_input)
+    if output>0.7:
+     output="Yes, you are having Heart Disease"
+    else:
+        output="No, you are not having Heart Disease"
+    # return("hello")  
+    return render_template("heart_disease_result.html",result=output)
+
+@app.route("/brain_tumor")
+def brain_tumor():
+    return render_template("brain_tumor.html")
+
+@app.route('/brain_tumor_result', methods=['POST'])
+def brain_tumor_result():
+    name = request.form['name']
+    age = request.form['age']
+    # img = request.form["image"]
+    img='1 no.jpeg'
+    path="brain_tumor_dataset/"+str(img)
+    img=cv2.imread(path)
+    img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (100,100))
+    user_input = img.reshape(1,10000)
+    model = models.load_model("Brain_tumor_Detection.h5")    
+    output=int(model.predict(user_input)[0])
+    if output==1:
+     output="Yes, you are having Brain Tumor"
+    else:
+        output="No, you are not having Brain Tumor"
+    im = Image.open(path)
+    data = io.BytesIO()
+    im.save(data, "JPEG")
+    encoded_img_data = base64.b64encode(data.getvalue())
+    return render_template("brain_tumor_result.html",result=output)
+
+@app.route("/breast_cancer")
+def breast_cancer():
+    return render_template("breast_cancer.html")
+
+@app.route('/breast_cancer_result', methods=['POST'])
+def breast_cancer_result():
+    mr = float(request.form['mean radius'])
+    mp = float(request.form['mean perimeter'])
+    ma = float(request.form['mean area'])
+    mc = float(request.form['mean compactness'])
+    mcy = float(request.form['mean concavity'])
+    mcp = float(request.form['mean concave points'])
+    user_input=[[mr,mp,ma,mc,mcy,mcp]]
+    print(user_input)
+    # model=""
+    # with open('Breast_Cancer_Model','rb') as file:
+    #   model = pickle.load(file)  
+    model = models.load_model("Breast_Cancer_Prediction.h5")
+    output=model.predict(user_input)[0]
+    if output>0.7:
+     output="Yes, you are having breast cancer"
+    else:
+        output="No, you are not having breast cancer"
+    return render_template("breast_cancer_result.html",result=output)    
+
+if __name__ == "__main__":
+    app.run(debug=True)
